f2a92e1e82f42a4b7d81e99423d38d45e6c562ab
abdullah <44318011+fx2y@users.noreply.github.com>
2023-10-10 17:11:13 +0700

    Define a hash function that takes a partitioning key and returns a hash value.

---
+++ data_ingestion/data_partitioning/consistent_hashing.py
@@ -0,0 +1,33 @@
+import hashlib
+
+
+class ConsistentHashing:
+    def __init__(self, nodes, replicas=3):
+        self.replicas = replicas
+        self.ring = dict()
+        for node in nodes:
+            for i in range(replicas):
+                key = self.hash(f"{node}:{i}")
+                self.ring[key] = node
+
+    def hash(self, key):
+        return int(hashlib.sha1(key.encode()).hexdigest(), 16)
+
+    def get_node(self, key):
+        if not self.ring:
+            return None
+        hash_key = self.hash(key)
+        for node in sorted(self.ring.keys()):
+            if hash_key <= node:
+                return self.ring[node]
+        return self.ring[min(self.ring.keys())]
+
+
+if __name__ == '__main__':
+    nodes = ['node1', 'node2', 'node3']
+    consistent_hashing = ConsistentHashing(nodes)
+
+    partitioning_keys = ['key1', 'key2', 'key3', 'key4', 'key5']
+    for key in partitioning_keys:
+        node = consistent_hashing.get_node(key)
+        print(f"Partitioning key '{key}' maps to node '{node}'")
