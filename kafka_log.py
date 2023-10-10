7614130bc2cc939b83e143dcd9a8c87fc15f00f4
abdullah <44318011+fx2y@users.noreply.github.com>
2023-10-10 15:30:41 +0700

    Move the Kafka producer initialization to a separate method, so that it can be reused by other methods if needed. Use a logger instead of print statements to handle errors and debug messages. Add docstrings to the methods and classes to improve code readability and maintainability.

--- kafka_log.py
+++ kafka_log.py
@@ -1,31 +1,60 @@
 import asyncio
+import logging
 
 from kafka import KafkaProducer
 
 
 class KafkaLog:
+    """
+    A class for ingesting data into a Kafka topic.
+    """
+
     def __init__(self, topic_name):
+        """
+        Initializes a Kafka producer for the given topic.
+
+        :param topic_name: The name of the Kafka topic.
+        """
+        self.producer = self._create_producer()
+        self.topic_name = topic_name
+
+    def _create_producer(self):
+        """
+        Creates a Kafka producer with the default configuration.
+
+        :return: A Kafka producer instance.
+        """
         try:
-            self.producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-            self.topic_name = topic_name
+            return KafkaProducer(bootstrap_servers=['localhost:9092'])
         except Exception as e:
-            # handle Kafka producer errors here
-            print(f"Error creating Kafka producer: {e}")
-            self.producer = None
+            logging.error(f"Error creating Kafka producer: {e}")
+            return None
 
     async def ingest(self, data):
+        """
+        Ingests the given data into the Kafka topic.
+
+        :param data: The data to be ingested.
+        """
         try:
-            # send the data to the Kafka topic
-            await self.producer.send(self.topic_name, data.encode('utf-8'))
+            future = self.producer.send(self.topic_name, data.encode('utf-8'))
+            result = await future.get()
+            logging.debug(f"Data ingested into Kafka topic {self.topic_name}: {result}")
         except Exception as e:
-            # handle ingestion errors here
-            print(f"Error ingesting data: {e}")
+            logging.error(f"Error ingesting data: {e}")
 
     def send(self, data):
+        """
+        Sends the given data to the Kafka topic.
+
+        :param data: The data to be sent.
+        """
         asyncio.run(self.ingest(data))
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
     # create an instance of the KafkaLog class
     log = KafkaLog('my_topic')
 
