5e77502ed6b579a6e319eb92a3f63534aa79d415
abdullah <44318011+fx2y@users.noreply.github.com>
2023-10-10 15:26:42 +0700

    allow the producer to send messages in a non-blocking way, which can improve the overall throughput of the system.

--- kafka_log.py
+++ kafka_log.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from kafka import KafkaProducer
 
 
@@ -11,19 +13,25 @@ class KafkaLog:
             print(f"Error creating Kafka producer: {e}")
             self.producer = None
 
-    def ingest(self, data):
+    async def ingest(self, data):
         try:
             # send the data to the Kafka topic
-            self.producer.send(self.topic_name, data.encode('utf-8'))
+            await self.producer.send(self.topic_name, data.encode('utf-8'))
         except Exception as e:
             # handle ingestion errors here
             print(f"Error ingesting data: {e}")
 
+    def send(self, data):
+        asyncio.run(self.ingest(data))
+
 
 if __name__ == '__main__':
     # create an instance of the KafkaLog class
-    log = KafkaLog('topic_name')
+    log = KafkaLog('my_topic')
 
-    # ingest some data
+    # send some data to the Kafka topic
     data = '{"name": "John", "age": 30}'
-    log.ingest(data)
+    log.send(data)
+
+    # wait for the data to be ingested
+    asyncio.run(asyncio.sleep(1))
