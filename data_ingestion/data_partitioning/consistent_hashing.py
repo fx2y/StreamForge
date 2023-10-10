2849214250f5d6482d7ee61c4574d109db0d6410
abdullah <44318011+fx2y@users.noreply.github.com>
2023-10-10 17:26:54 +0700

    allow for dynamic addition and removal of nodes from the cluster. This will enable the system to scale horizontally by adding more nodes to the cluster as the data volume increases.

--- data_ingestion/data_partitioning/consistent_hashing.py
+++ data_ingestion/data_partitioning/consistent_hashing.py
@@ -22,12 +22,39 @@ class ConsistentHashing:
                 return self.ring[node]
         return self.ring[min(self.ring.keys())]
 
+    def add_node(self, node):
+        for i in range(self.replicas):
+            key = self.hash(f"{node}:{i}")
+            self.ring[key] = node
+
+    def remove_node(self, node):
+        keys_to_remove = []
+        for key, value in self.ring.items():
+            if value == node:
+                keys_to_remove.append(key)
+        for key in keys_to_remove:
+            del self.ring[key]
+
 
 if __name__ == '__main__':
     nodes = ['node1', 'node2', 'node3']
     consistent_hashing = ConsistentHashing(nodes)
 
     partitioning_keys = ['key1', 'key2', 'key3', 'key4', 'key5']
+    for key in partitioning_keys:
+        node = consistent_hashing.get_node(key)
+        print(f"Partitioning key '{key}' maps to node '{node}'")
+
+    consistent_hashing.add_node('node4')
+    node = consistent_hashing.get_node('key1')
+    print(f"Partitioning key 'key1' maps to node '{node}' after adding node4")
+    for key in partitioning_keys:
+        node = consistent_hashing.get_node(key)
+        print(f"Partitioning key '{key}' maps to node '{node}'")
+
+    consistent_hashing.remove_node('node1')
+    node = consistent_hashing.get_node('key1')
+    print(f"Partitioning key 'key1' maps to node '{node}' after removing node1")
     for key in partitioning_keys:
         node = consistent_hashing.get_node(key)
         print(f"Partitioning key '{key}' maps to node '{node}'")
