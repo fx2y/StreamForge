ce295a65a083a00bbe823345738c57b9e19a6eb3
abdullah <44318011+fx2y@users.noreply.github.com>
2023-10-10 15:38:43 +0700

    use a more performant HTTP server implementation like gunicorn

--- http_adapter.py
+++ http_adapter.py
@@ -1,4 +1,6 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler
+
+from gunicorn.app.base import BaseApplication
 
 
 class HTTPAdapter:
@@ -6,8 +8,28 @@ class HTTPAdapter:
         self.port = port
 
     def start(self):
-        server = HTTPServer(('', self.port), HTTPRequestHandler)
-        server.serve_forever()
+        class HTTPApplication(BaseApplication):
+            def __init__(self, app, options=None):
+                self.options = options or {}
+                self.application = app
+                super().__init__()
+
+            def load_config(self):
+                for key, value in self.options.items():
+                    if key in self.cfg.settings and value is not None:
+                        self.cfg.set(key.lower(), value)
+
+            def load(self):
+                return self.application
+
+        options = {
+            'bind': f'0.0.0.0:{self.port}',
+            'workers': 4,  # adjust this value based on your system resources
+            'worker_class': 'sync',  # use sync worker class for simplicity
+        }
+
+        # use gunicorn
+        HTTPApplication(HTTPRequestHandler, options).run()
 
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
@@ -15,8 +37,20 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         content_length = int(self.headers['Content-Length'])
         data = self.rfile.read(content_length)
         # Add deserialization logic here
-        self.send_response(200)
-        self.end_headers()
+        try:
+            deserialized_data = self.deserialize_data(data)
+            self.send_response(200)
+        except Exception as e:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(str(e).encode())
+        else:
+            self.end_headers()
+
+    def deserialize_data(self, data):
+        # Add deserialization logic here
+        return data
 
 
 if __name__ == '__main__':
