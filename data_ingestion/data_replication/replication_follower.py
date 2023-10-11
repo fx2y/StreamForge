3fd565dbe11105f6cc7186c9a9e632c37009708f
abdullah <44318011+fx2y@users.noreply.github.com>
2023-10-11 18:46:49 +0700

    Implement a protocol for the follower to receive data from the leader and ensure it is in the correct order.

---
+++ data_ingestion/data_replication/replication_follower.py
@@ -0,0 +1,68 @@
+import zlib
+from multiprocessing import Manager
+
+class ReplicationFollower:
+    def __init__(self, leader):
+        """
+        Initialize the replication follower with a reference to the replication leader.
+        """
+        self.leader = leader
+        self.local_log = Manager().list()
+        self.last_sequence_number = -1
+
+    def receive_batch(self, batch):
+        """
+        Receive a batch of data from the replication leader and store it in the local log.
+        """
+        for sequence_number, record in batch:
+            if sequence_number == self.last_sequence_number + 1:
+                decompressed_data = zlib.decompress(record)
+                # Store decompressed data in local log
+                self.local_log.append(decompressed_data)
+                self.last_sequence_number = sequence_number
+            elif sequence_number > self.last_sequence_number + 1:
+                self.leader.recover()
+                self.last_sequence_number = self.leader.sequence_number - 1
+                break
+
+    def handle_failures(self):
+        """
+        Handle failures of the leader and elect a new leader.
+        """
+        if not self.leader.is_alive():
+            self.leader = self._elect_new_leader()
+
+    def _elect_new_leader(self):
+        """
+        Elect a new leader from the remaining replicas.
+        """
+        for replica in self.leader.replicas:
+            if replica != self.leader:
+                return replica
+        raise Exception('No available replicas')
+
+    def handle_network_partitions(self):
+        """
+        Handle network partitions and ensure that the follower can still communicate with the leader.
+        """
+        if not self.leader.is_alive():
+            self.leader = self._get_new_leader()
+
+    def _get_new_leader(self):
+        """
+        Get a new leader from the remaining replicas.
+        """
+        for replica in self.leader.replicas:
+            if replica != self.leader:
+                try:
+                    replica.ping()
+                    return replica
+                except:
+                    pass
+        raise Exception('No available replicas')
+
+    def ping(self):
+        """
+        Ping the leader to check if it is still alive.
+        """
+        return True
