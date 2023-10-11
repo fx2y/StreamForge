760a51ccfd22bb8207462e06a96163e233bbc6e4
abdullah <44318011+fx2y@users.noreply.github.com>
2023-10-11 14:21:22 +0700

    Add documentation for the replication_protocol.py

--- data_ingestion/data_replication/replication_protocol.py
+++ data_ingestion/data_replication/replication_protocol.py
@@ -5,11 +5,17 @@ from multiprocessing import Pool
 
 class ReplicationProtocol:
     def __init__(self, replicas):
+        """
+        Initialize the replication protocol with a list of replicas.
+        """
         self.replicas = replicas
         self.leader = None
         self.batch_size = 100  # Set batch size to 100 for example purposes
 
     def elect_leader(self):
+        """
+        Elect a leader among the replicas.
+        """
         self.leader = self.replicas[0]  # Select first replica as leader for simplicity
         return self.leader
 
@@ -21,6 +27,9 @@ class ReplicationProtocol:
             time.sleep(1)
 
     def send_data(self, data):
+        """
+        Send data to the leader, compressing and batching it for performance optimization.
+        """
         compressed_data = zlib.compress(data.encode())
         batch = []
         for record in compressed_data:
@@ -32,13 +41,22 @@ class ReplicationProtocol:
             self.leader.send_batch(batch)
 
     def receive_batch(self, batch):
+        """
+        Receive a batch of data from the leader and distribute it to the followers in parallel.
+        """
         with Pool(len(self.replicas)) as p:
             p.map(self._receive_batch, [(replica, batch) for replica in self.replicas if replica != self.leader])
 
     def _receive_batch(self, replica, batch):
+        """
+        Receive a batch of data from the leader and store it in the local log.
+        """
         replica.receive_batch(batch)
 
     def receive_data(self, data):
+        """
+        Receive data from the leader, decompressing it and storing it in the local log.
+        """
         decompressed_data = zlib.decompress(data)
         # Store decompressed data in local log
         pass
