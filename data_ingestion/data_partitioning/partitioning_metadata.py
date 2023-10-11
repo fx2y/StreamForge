9b7177a82b984fe9477155eea5a4a3d7e40d5cad
abdullah <44318011+fx2y@users.noreply.github.com>
2023-10-11 12:34:17 +0700

    Define a schema for the metadata store that includes the node ID, partition ID, and replica IDs.

--- data_ingestion/data_partitioning/partitioning_metadata.py
+++ data_ingestion/data_partitioning/partitioning_metadata.py
@@ -1,17 +1,65 @@
+import sqlite3
+
+
 class PartitioningMetadata:
-    def __init__(self):
-        self.metadata = dict()
+    def __init__(self, db_file):
+        self.conn = sqlite3.connect(db_file)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS partition_replicas (
+                node_id TEXT,
+                partition_id TEXT,
+                replica_id TEXT,
+                PRIMARY KEY (node_id, partition_id, replica_id)
+            )
+        ''')
+        self.conn.commit()
 
-    def add_partition(self, partition_id, node_id, replicas):
-        if node_id not in self.metadata:
-            self.metadata[node_id] = dict()
-        self.metadata[node_id][partition_id] = replicas
+    def add_replica(self, node_id, partition_id, replica_id):
+        self.cursor.execute('''
+            INSERT INTO partition_replicas (node_id, partition_id, replica_id)
+            VALUES (?, ?, ?)
+        ''', (node_id, partition_id, replica_id))
+        self.conn.commit()
 
-    def remove_partition(self, partition_id, node_id):
-        if node_id in self.metadata and partition_id in self.metadata[node_id]:
-            del self.metadata[node_id][partition_id]
+    def remove_replica(self, node_id, partition_id, replica_id):
+        self.cursor.execute('''
+            DELETE FROM partition_replicas
+            WHERE node_id = ? AND partition_id = ? AND replica_id = ?
+        ''', (node_id, partition_id, replica_id))
+        self.conn.commit()
+
+    def get_partition_nodes(self, partition_id):
+        self.cursor.execute('''
+            SELECT DISTINCT node_id
+            FROM partition_replicas
+            WHERE partition_id = ?
+        ''', (partition_id,))
+        return [row[0] for row in self.cursor.fetchall()]
 
     def get_node_partitions(self, node_id):
-        if node_id in self.metadata:
-            return self.metadata[node_id]
-        return dict()
+        self.cursor.execute('''
+            SELECT DISTINCT partition_id
+            FROM partition_replicas
+            WHERE node_id = ?
+        ''', (node_id,))
+        return [row[0] for row in self.cursor.fetchall()]
+
+    def get_partition_replicas(self, partition_id):
+        self.cursor.execute('''
+            SELECT replica_id
+            FROM partition_replicas
+            WHERE partition_id = ?
+        ''', (partition_id,))
+        return [row[0] for row in self.cursor.fetchall()]
+
+    def add_partition(self, partition_id, node_id, replicas):
+        for replica_id in replicas:
+            self.add_replica(node_id, partition_id, replica_id)
+
+    def remove_partition(self, partition_id, node_id):
+        self.cursor.execute('''
+            DELETE FROM partition_replicas
+            WHERE node_id = ? AND partition_id = ?
+        ''', (node_id, partition_id))
+        self.conn.commit()
