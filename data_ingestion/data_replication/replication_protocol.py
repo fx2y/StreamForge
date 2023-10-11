390cdacb93487f659eb387ad5f28f2a627b7656a
abdullah <44318011+fx2y@users.noreply.github.com>
2023-10-11 14:01:41 +0700

    Implement a batching mechanism that can group multiple data records into a single message to reduce network overhead.

--- data_ingestion/data_replication/replication_protocol.py
+++ data_ingestion/data_replication/replication_protocol.py
@@ -5,6 +5,7 @@ class ReplicationProtocol:
     def __init__(self, replicas):
         self.replicas = replicas
         self.leader = None
+        self.batch_size = 100  # Set batch size to 100 for example purposes
 
     def elect_leader(self):
         self.leader = self.replicas[0]  # Select first replica as leader for simplicity
@@ -18,9 +19,19 @@ class ReplicationProtocol:
             time.sleep(1)
 
     def send_data(self, data):
+        batch = []
+        for record in data:
+            batch.append(record)
+            if len(batch) == self.batch_size:
+                self.leader.send_batch(batch)
+                batch = []
+        if batch:
+            self.leader.send_batch(batch)
+
+    def receive_batch(self, batch):
         for replica in self.replicas:
             if replica != self.leader:
-                replica.receive_data(data)
+                replica.receive_batch(batch)
 
     def receive_data(self, data):
         # Store data in local log
