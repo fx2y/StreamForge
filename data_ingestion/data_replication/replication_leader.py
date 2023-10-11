69f0fd4ba53840814f8577bf819887e358559183
abdullah <44318011+fx2y@users.noreply.github.com>
2023-10-11 14:55:10 +0700

    Implement a protocol for the leader to send data to the followers and ensure they have received it.

--- data_ingestion/data_replication/replication_leader.py
+++ data_ingestion/data_replication/replication_leader.py
@@ -11,6 +11,7 @@ class ReplicationLeader:
         self.batch_size = 100  # Set batch size to 100 for example purposes
         self.sequence_number = 0
         self.data_buffer = Manager().list()
+        self.acknowledgements = Manager().dict()
 
     def receive_data(self, data):
         """
@@ -38,6 +39,7 @@ class ReplicationLeader:
                 shared_batch = manager.list(batch)
                 with Pool(len(self.replicas)) as p:
                     p.map(self._send_batch, [(replica, shared_batch) for replica in self.replicas if replica != self])
+            self._wait_for_acknowledgements(batch)
 
     def _send_batch(self, replica, shared_batch):
         """
@@ -45,6 +47,22 @@ class ReplicationLeader:
         """
         batch = list(shared_batch)
         replica.receive_batch(batch)
+
+    def _wait_for_acknowledgements(self, batch):
+        """
+        Wait for acknowledgements from the followers for a batch of data.
+        """
+        for sequence_number, _ in batch:
+            while True:
+                if all(acknowledgements.get(sequence_number, False) for acknowledgements in
+                       self.acknowledgements.values()):
+                    break
+
+    def receive_acknowledgement(self, replica_id, sequence_number):
+        """
+        Receive an acknowledgement from a follower for a sequence number.
+        """
+        self.acknowledgements[replica_id][sequence_number] = True
 
     def receive_batch(self, batch):
         """
@@ -54,3 +72,12 @@ class ReplicationLeader:
             decompressed_data = zlib.decompress(record)
             # Store decompressed data in local log
             pass
+        self._send_acknowledgements(batch)
+
+    def _send_acknowledgements(self, batch):
+        """
+        Send acknowledgements to the followers for a batch of data.
+        """
+        for replica in self.replicas:
+            if replica != self:
+                replica.receive_acknowledgement(self.replicas.index(self), batch[-1][0])
